2e0ccdf42a1f3602806f3b80001f243cf02be31f
cha-nox <val8ioks@gmail.com>
2025-12-03 15:14:13 +0100

    feat: Added React Router.
    - Added React Router for routes management.
    - Moved the current home page into its very own component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,13 @@
-import { useState, useEffect } from "react";
+import {createBrowserRouter, RouterProvider}    from 'react-router-dom';
+import Home                                     from './components/Home/Home';
+import './App.css';
 
-import MarkdownEditor from "./components/Editor/MarkdownEditor";
-import MarkdownPreview from "./components/Editor/MarkdownPreview";
-import FileTree from "./components/Sidebar/FileTree";
-import "./App.css";
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home/>,
+        errorElement: <div>Error</div>,
+    },
+]);
 
-
-export default function App() {
-
-  // Charger le texte sauvegardé au démarrage
-  const [files, setFiles] = useState(() => {
-    const savedFiles = localStorage.getItem("markdown-files");
-    return savedFiles ? JSON.parse(savedFiles) : [
-      { id: 1, name: "notes.md", content: "content 1 ", type: 'file', parentID: 'null' },
-      { id: 2, name: "todo.md", content: "content 2", type: 'file', parentID: 'null' },
-      { id: 3, name: "ideas.md", content: "content 3", type: 'file', parentID: 'null' }
-    ];
-  });
-
-
- 
-  const [activeFileId, setActiveFileId] = useState(1);
-
-  //  trouver le fichier Actif 
-  const activeFile = files.find(file => file.id === activeFileId);
-
-
-  // chnange contenu du fichier actif 
-  const changeContent = (newContent) => {
-    setFiles(files.map(file => file.id === activeFileId ? { ...file, content: newContent } : file
-    ));
-  }
-
-  // Sauvegarder automatiquement à chaque modification
-  useEffect(() => {
-    localStorage.setItem("markdown-files", JSON.stringify(files));
-  }, [files]);
-
-  return (
-    <>
-      <h1>Éditeur MarkDown</h1>
-
-
-      <div className="app_container">
-        <aside className="sidebar">
-          <h2>Mes fichiers</h2>
-
-          <FileTree
-            files={files}
-            activeFileId={activeFileId}
-            onFileSelect={setActiveFileId}
-          />
-        </aside>
-
-
-
-        <div className="editor-layout">
-          <div className="editor-container">
-            <MarkdownEditor value={activeFile.content} onChange={changeContent} />
-          </div>
-          <div className="preview-container">
-            <MarkdownPreview text={activeFile.content} />
-          </div>
-        </div>
-      </div>
-
-
-    </>
-  );
-}
+export default function App(){ return <RouterProvider router = {router} />; };
